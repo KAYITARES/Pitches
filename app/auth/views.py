f05fc76d84b1754268771796bd2871ea7559495e
MBOYIRE <mboyire14@gmail.com>
2019-02-27 12:08:26 +0200

    adding send-email function in email.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,7 @@ from flask import render_template,redirect,url_for, flash,request
 from flask_login import login_user,logout_user,login_required
 from ..models import User
 from .forms import LoginForm,RegistrationForm
+from ..email import mail_message
 
 from .. import db
 
@@ -18,7 +19,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Pitches login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -28,6 +29,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
+        mail_message("Welcome to Pitches","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-export MOVIE_API_KEY=b8e19cc3ea78b217ae0eddb1a13646d1
+export MAIL_USERNAME=cyntkayitare
+export MAIL_PASSWORD=pmfmsnclb
 export SECRET_KEY=12345
 
 python3.6 manage.py server
